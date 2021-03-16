4c028cb6594a9ffac1d8db94f45d21758f01940a
Abhishek Lad <abhisheklad12@gmail.com>
2021-03-16 18:00:33 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# New Relic Custom Visualizations
+
+# Visualizations
+
+### NRQL Status Widget
+
+![NRQL Area Chart with Thresholds](timeseries-threshold/images/areachart-thresholds1.png)
+
+## Getting started
+
+Run the following scripts:
+
+```
+npm install
+npm start
+```
+
+Visit https://one.newrelic.com/?nerdpacks=local and :sparkles:
+
+## Dashboard deployment
+
+https://developer.newrelic.com/build-apps/build-visualization#deploy-and-use-your-visualization
+
+```
+This needs to be deployed into your account so that the custom visualizations can be added to dashboards.
+After this has been deployed, ensure you navigate to the app and add to the account.
+```
+
+## Documentation
+
+https://developer.newrelic.com/build-apps/build-visualization#deploy-and-use-your-visualization
+
+## Creating new artifacts
+
+If you want to create new artifacts run the following command:
+
+```
+nr1 create
+```
+
+> Example: `nr1 create --type nerdlet --name my-nerdlet`.
---
+++ visualizations/timeseries-threshold/index.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import _ from 'lodash';
+import { Card, CardBody, HeadingText, NrqlQuery, PlatformStateContext, Spinner, AutoSizer } from 'nr1';
+
+export default class TimeseriesThresholdVisualization extends React.Component {
+
+  static propTypes = {
+    accountId: PropTypes.number,
+    query: PropTypes.string,
+    title: PropTypes.string,
+    critical: PropTypes.string,
+    warning: PropTypes.string,
+    yAxis: PropTypes.string,
+    MouseoverTooltip: PropTypes.string,
+    chartType: PropTypes.bool
+  };
+
+  transformData = (result) => {
+    var rawDataPoints = _.mapValues(result, 'data');
+    var finalDataSet = [];
+    _.map(rawDataPoints, function (k) {
+      _.map(k, function (i) {
+        var tmpArray = [];
+        tmpArray.push(i.x, i.y);
+        finalDataSet.push(tmpArray);
+      });
+    });
+    return finalDataSet;
+  };
+
+  render() {
+
+    const { accountId, query, warning, critical, title, yAxis, MouseoverTooltip, chartType } = this.props;
+    if (!query) {
+      return <EmptyState />;
+    }
+
+    return (
+      <PlatformStateContext.Consumer>
+        {({ timeRange }) => (
+          <AutoSizer>
+            {({ width, height }) => (
+              <NrqlQuery
+                query={query}
+                accountId={parseInt(accountId)}
+                timeRange={timeRange}
+                pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
+              >
+                {({ data, loading, error }) => {
+                  if (loading) {
+                    return <Spinner />;
+                  }
+
+                  if (error) {
+                    // Throwing an error displays an error state consistent with other
+                    // visualizations.
+                    throw new Error(error.message);
+                  }
+
+                  var transformedData = this.transformData(data);
+
+                  Highcharts.setOptions({
+                    global: {
+                      useUTC: false
+                    }
+                  });
+
+                  var options = {
+                    chart: {
+                      zoomType: 'x',
+                      type: chartType ? 'area' : 'line'
+                    },
+                    legend: {
+                      enabled: false
+                    },
+                    title: {
+                      text: title,
+                      align: 'left'
+                    },
+                    xAxis: {
+                      type: 'datetime'
+                    },
+                    yAxis: {
+                      min: 0,
+                      title: {
+                        text: yAxis
+                      },
+                      plotLines: [{
+                        color: '#FF0000',
+                        width: 2,
+                        value: parseFloat(critical)
+                      }, {
+                        color: '#F5B038',
+                        width: 2,
+                        dashStyle: 'shortdash',
+                        value: parseFloat(warning)
+                      }]
+                    },
+                    plotOptions: {
+                      line: {
+                        dataLabels: {
+                          enabled: false
+                        },
+                        marker: {
+                          enabled: false,
+                          states: {
+                            hover: {
+                              enabled: false
+                            }
+                          }
+                        }
+                      }
+                    },
+                    series: [
+                      {
+                        name: MouseoverTooltip,
+                        data: transformedData
+                      }, {
+                        name: MouseoverTooltip,
+                        data: transformedData,
+                        color: '#F5B038',
+                        negativeColor: 'transparent',
+                        threshold: parseFloat(warning)
+                      }, {
+                        //The CRITICAL threshold should come after in the array
+                        name: MouseoverTooltip,
+                        data: transformedData,
+                        color: '#FF0000',
+                        negativeColor: 'transparent',
+                        threshold: parseFloat(critical)
+                      },]
+                  };
+                  return (
+                    <HighchartsReact
+                      highcharts={Highcharts}
+                      options={options}
+                    />
+                  );
+                }}
+              </NrqlQuery>
+            )}
+          </AutoSizer>
+        )}
+      </PlatformStateContext.Consumer>
+    );
+  }
+}
+
+const EmptyState = () => (
+  <Card className="EmptyState-Card">
+    <CardBody className="EmptyState-Card-Body">
+      <HeadingText
+        spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+        type={HeadingText.TYPE.HEADING_3}
+      >
+        Please provide at least one NRQL query & account ID pair
+      </HeadingText>
+      <HeadingText
+        spacingType={[HeadingText.SPACING_TYPE.MEDIUM]}
+        type={HeadingText.TYPE.HEADING_4}
+      >
+        An example NRQL query you can try is:
+      </HeadingText>
+      <code>SELECT average(cpuPercent) FROM ProcessSample SINCE 1 DAY AGO TIMESERIES</code>
+    </CardBody>
+  </Card>
+);
